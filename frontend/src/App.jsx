dbc9c4e13c5c17e2b4478b36833cba5be81b9ee5
LinhXinh <tranthilinhhh17@gmail.com>
2025-08-30 01:22:43 +0700

    Cập nhật cấu trúc frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';  // Import CSS cho component này
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Gift Shop</h1>
+      <p>Discover amazing products!</p>
+    </div>
+  );
+}
+
+export default App;
